cedd0b139f5e6ac32bfa7b8c539f8834a97b72f2
Gabriel Moraga <agarom97@gmail.com>
2021-01-18 15:40:00 +0100

    Secondo esercizio

--- js/script.js
+++ js/script.js
@@ -10,3 +10,99 @@ for (var key in primoStudente) {
   console.log("Il/L'" + key + ' è ' + primoStudente[key] + '.');
 }
 // -----------------------------------------------------------------------------
+
+// Secondo esercizio -----------------------------------------------------------
+var classe = [
+  {
+    'Nome' : 'Gabriel',
+    'Cognome' : 'Moraga Figueroa'
+  },
+
+  {
+    'Nome' : 'Lorenzo',
+    'Cognome' : 'Morelli'
+  },
+
+  {
+    'Nome' : 'Michele',
+    'Cognome' : 'Poggi'
+  },
+
+  {
+    'Nome' : 'Marco',
+    'Cognome' : 'Madia'
+  },
+
+  {
+    'Nome' : 'Marco',
+    'Cognome' : 'Gattuso'
+  },
+
+  {
+    'Nome' : 'Matt',
+    'Cognome' : 'Aster'
+  },
+
+  {
+    'Nome' : 'Gianandrea',
+    'Cognome' : 'Muià'
+  },
+
+  {
+    'Nome' : 'Nicola',
+    'Cognome' : 'Degobbis'
+  },
+
+  {
+    'Nome' : 'Gabrio',
+    'Cognome' : 'Pozzi'
+  },
+
+  {
+    'Nome' : 'Giulia',
+    'Cognome' : 'Bersani'
+  },
+
+  {
+    'Nome' : 'Sara',
+    'Cognome' : 'Stefanizzi'
+  },
+
+  {
+    'Nome' : 'Luca',
+    'Cognome' : 'Occhi'
+  },
+
+  {
+    'Nome' : 'Andrea',
+    'Cognome' : 'Guagnini'
+  },
+
+  {
+    'Nome' : 'Simone',
+    'Cognome' : 'Rosini'
+  },
+
+  {
+    'Nome' : 'Tommaso',
+    'Cognome' : 'Amitrano'
+  },
+
+  {
+    'Nome' : 'Mattia',
+    'Cognome' : 'Ceniti'
+  }
+]
+
+console.log(classe);
+
+for (var i = 0; i < classe.length; i++) {
+  console.log('Studente ' + (i+1));
+
+  for (var key in classe[i]) {
+    console.log(key + ': ' + classe[i][key]);
+  }
+
+  console.log(classe[i]);
+}
+// -----------------------------------------------------------------------------
